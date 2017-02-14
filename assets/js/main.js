adc2c014462964df8552c78fece4f683defc83ec
Galish <galish.ufa@gmail.com>
2017-02-14 13:32:31 +0500

    Spinner added

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,8 @@ class App extends React.Component {
 		this.state = {
 			search: [],
 			selected: {},
-			query: ''
+			query: '',
+			isLoading: false
 		}
 	}
 
@@ -21,24 +22,32 @@ class App extends React.Component {
 
 	onSubmit = (e) => {
 		e.preventDefault()
-		const {query} = this.state
+		const {query, isLoading} = this.state
 
-		if (!query) return
+		if (!query || isLoading) return
+
+		this.setState({isLoading: true})
 
 		actions.onSearch(query).end((err, res) => {
 			if (err) {
 				return console.log(err);
 			}
-			this.setState({search: res.body})
+			this.setState({
+				search: res.body,
+				isLoading: false
+			})
 		})
 	}
 
 	onSelect = (selected) => {
+		const {query} = this.state
 		this.setState({
 			selected,
 			search: []
 		})
-		console.log({selected});
+		console.log('QUERY: ', query)
+		console.log('RESUL: ', selected)
+		console.log('=====')
 		this.renderObjectOnMap(selected)
 	}
 
@@ -48,6 +57,28 @@ class App extends React.Component {
 			query: value,
 			search: []
 		})
+	}
+
+	renderSearchForm = () => {
+		const {query} = this.state
+
+		return (
+			<form className="search__form"
+				onSubmit={this.onSubmit}>
+				<label className="search__label">
+					Search:
+				</label>
+				<input className="search__input"
+					onChange={this.onTextChange}
+					placeholder="Search..."
+					type="text"
+					value={query} />
+				<input type="submit"
+					className="search__submit"
+					value="..." />
+				{this.renderSearchResults()}
+			</form>
+		)
 	}
 
 	renderSearchResults = () => {
@@ -88,7 +119,7 @@ class App extends React.Component {
 	initMap = () => {
 		this.map = L.map('map').setView([51.505, -0.09], 13)
 
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+		L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -108,9 +139,9 @@ class App extends React.Component {
 
 		if (!isEmpty(geojson)) {
 			var myStyle = {
-				// "color": "#ff7800",
-				// "weight": 5,
-				// "opacity": 0.65
+				"color": "#20b5e1",
+				"weight": 2,
+				"opacity": 0.9
 			}
 
 			L.geoJSON([geojson], {style: myStyle}).addTo(this.map);
@@ -123,28 +154,29 @@ class App extends React.Component {
 		)
 	}
 
-	render () {
-		const {query} = this.state
+	renderSpinner = () => {
+		const {isLoading} = this.state
+
+		if (!isLoading) return null
 
 		return (
+			<div className="spinner">
+				<div className="rect1" />
+				<div className="rect2" />
+				<div className="rect3" />
+				<div className="rect4" />
+				<div className="rect5" />
+			</div>
+		)
+	}
+
+	render () {
+		return (
 			<div className="search">
-				<form className="search__form"
-					onSubmit={this.onSubmit}>
-					<label className="search__label">
-						Search:
-					</label>
-					<input className="search__input"
-						onChange={this.onTextChange}
-						placeholder="Search..."
-						type="text"
-						value={query} />
-					<input type="submit"
-						className="search__submit"
-						value="..." />
-					{this.renderSearchResults()}
-				</form>
+				{this.renderSearchForm()}
 				{this.renderDetails()}
 				{this.renderMap()}
+				{this.renderSpinner()}
 			</div>
 		)
 	}
